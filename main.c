24cd54094f4e0c11a9b4ef9d4c74dfa173a0ee13
Galdino <pedro.wollmann@gmail.com>
2024-06-16 21:23:12 -0300

    implementado metodo de verificar quantos personagens estao vivos e se o jogo devera acabar

--- main.c
+++ main.c
@@ -8,20 +8,25 @@
     };
     typedef struct characters ch;
 int main(){
+    //initialiazing the variables
     int language;
     ch frieren = {100,50,50,12}, warrior={150,60,40,14}, heiter={60,13,53,13}, king_demon= {100,53,53,63};
     int charac =3;
+
+    // choosing a preferred language
     printf("Escolha a sua ligua/choose your language");
     printf("[1]-English/[2]-portugues");
     do{
     scanf("%d", &language);
     }while(language ==1 || language == 2);
 
+    // actual game
     while (1){
-        if (charac == 0){
+        
+        int s = status_party(&charac, frieren.hp,warrior.hp,heiter.hp);
+        if (s == 0){
             break;
         }
-        status_party();
 
 
 
