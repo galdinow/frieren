344a804d04a1e2ced4957154aa48c58420e00b12
Galdino <pedro.wollmann@gmail.com>
2024-06-29 14:28:09 -0300

    adicionada a mecanica da mochila

--- main.c
+++ main.c
@@ -7,19 +7,27 @@ int main(){
     //initialiazing the variables
     int language;
     int escolha;
-    ch frieren = {"frieren",100,100,50,50,12, "zoltrak","fireball", "batata"}
-    , eisen={"eisen",150,150,60,40,14,"cabecada", "furia do anao","soco"},
-     heiter={"heiter",60,60,13,53,13,"ataque da deusa", "furia da deusa", "ataque de luz"},
-     himmel={"himmel", 60,60,13,53,13, "Avanco do heroi", "charme do heroi", "corte profundo"},
-      king_demon= {"king",1000,1000,53,53,63,"arroz", "ice ball", "mud"};
+
+    //inicializzacao dos personagens
+    ch frieren = {"Frieren",200,200,50,50,12, "zoltrak","fireball", "batata"}
+    , eisen={"Eisen",500,500,60,40,14,"cabecada", "furia do anao","soco"},
+     heiter={"Heiter",200,200,13,53,13,"ataque da deusa", "furia da deusa", "ataque de luz"},
+     himmel={"Himmel", 300,300,13,53,13, "Avanco do heroi", "charme do heroi", "corte profundo"},
+      king_demon= {"King",1000,1000,53,53,63,"arroz", "ice ball", "mud"};
 
     int party_alive =4;
     ch *characters[5] = {&frieren, &heiter, &eisen,&himmel, &king_demon};
 
-    /*bg frieren_bag = {"frieren", "mistery potion", "rock", "water"}
-    , warrior_bag={"warrior", "sla", "aa","ab"},
-     heiter_bag= {"heiter", "afa","gag", "fsfsv"}, king_demon_bag={"king demon", "afss","gnknkmk","kmsdkms"};
-     bg characters_bag[4]={frieren_bag,heiter_bag,warrior_bag,king_demon_bag};*/
+    //inicializacao dos itens
+    bg frieren_bag = {"mistery potion",3, "rock",2, "himmel's ring",5}
+    , eisen_bag={"Machado",1, "Grande espada",1,"Clave",1},
+     heiter_bag= {"bolo de chocolate",1,"pocao", 1,"maldicao",1}, 
+     king_demon_bag={"demon sword",1,"healing barrel",1,"poison",1},
+     himmel_bag= {"lagrima do elfo",1,"espada do heroi",1,"pose heroica",100};
+
+     bg *characters_bag[4]={&frieren_bag,&heiter_bag,&eisen_bag,&king_demon_bag};
+
+
     // choosing a preferred language
     /*printf("Escolha a sua ligua/choose your language\n");
     printf("[1]-English/[2]-portugues");
@@ -38,19 +46,19 @@ int main(){
     while (1){
         system("clear");
         int escolha =0;
-        int attack =0;
-        
-        /*if (status_party(&party_alive, frieren.hp,warrior.hp,heiter.hp) == 0){
-            break;
-        }*/
-    //frieren.hp -= 20;
-        
+        int attack;
+        int item;
+        //verifica se o boss esta vivo
         if(king_demon.hp <0){
             king_demon.hp = 0;
-            return 0;
+            system("clear");
+            printf("Parabens, voce derrotou o rei demonio. Uma nova era de paz surgira");
 
         }
-        members(&frieren,&himmel,&heiter,&eisen);
+        //verifica quantos aliados estao vivos
+        if(members(&frieren,&himmel,&heiter,&eisen) == 0){
+            return 0;
+        }
 
         escolha = menu(characters[cont],&king_demon);
         if (escolha == 1){
@@ -62,10 +70,10 @@ int main(){
                     king_demon.hp -= 50;
                     
                 }
-                if (attack == 2){
+                else if (attack == 2){
                     king_demon.hp -= 100;
                 }
-                if (attack == 3){
+                else if (attack == 3){
                     king_demon.hp -= 110;
                 }
            }
@@ -74,10 +82,10 @@ int main(){
                 if(attack == 1){
                     king_demon.hp -= 50;
                 }
-                if (attack == 2){
+               else if (attack == 2){
                     king_demon.hp -= 100;
                 }
-                if (attack == 3){
+                else if (attack == 3){
                     king_demon.hp -= 110;
                 }
            }
@@ -86,10 +94,10 @@ int main(){
                 if(attack == 1){
                     king_demon.hp -= 50;
                 }
-                if (attack == 2){
+                else if (attack == 2){
                     king_demon.hp -= 100;
                 }
-                if (attack == 3){
+                else if (attack == 3){
                     king_demon.hp -= 110;
                 }
            }
@@ -97,10 +105,10 @@ int main(){
                 if(attack == 1){
                     king_demon.hp -= 50;
                 }
-                if (attack == 2){
+                else if (attack == 2){
                     king_demon.hp -= 100;
                 }
-                if (attack == 3){
+                else if (attack == 3){
                     king_demon.hp -= 110;
                 }
            }
@@ -108,8 +116,57 @@ int main(){
 
         }
         else if(escolha == 2){
-            //clear screen
-            //menu_bag(&characters[cont]);
+            system("clear");
+            item =menu_bag(characters_bag[cont],characters[cont]);
+            if (cont==0){
+                if(item ==1){
+                    frieren.hp += 20;
+                    frieren_bag.uses1--;
+                }
+                if(item==2){
+                    king_demon.hp-=500;
+                }
+                if(item==3){
+                    frieren.hp+=5;
+                }
+            }
+            else if(cont ==1){
+                if(item == 1){
+                    //cura em grupo
+                    frieren.hp+=20;
+                    eisen.hp+=20;
+                    himmel.hp+=20;
+                }
+                if(item == 2){
+                    heiter.hp +=100;
+                }
+                if(item==3){
+                    king_demon.hp -=150;
+                }
+
+            }
+            else if(cont == 2){
+                if(item == 1){
+                    king_demon.hp-=150;
+                }
+                if(item == 2){
+                    king_demon.hp-=200;
+                }
+                if(item==3){
+                    king_demon.hp -=400;
+                }
+            }
+            else if(cont == 3){
+                if(item == 1 ){
+                    himmel.hp+=100;
+                }
+                if(item == 2){
+                    king_demon.hp-=200;
+                }
+                else if(item==3 ){
+                    himmel.hp+=1;
+                }
+            }
 
         }
         else if(escolha == 3){
@@ -118,7 +175,7 @@ int main(){
 
         }
         else{
-            //resetar o game
+            return 0;
 
         }
 
@@ -132,8 +189,9 @@ int main(){
         srand(time(NULL)+5);
         int rn_attack= (rand() % (upper1-lower1+1)) +lower1;
         kings_turn(rn_party,rn_attack,&frieren, &himmel, &heiter,&eisen, &king_demon);
+
         cont++;
-        if(cont ==3){
+        if(cont ==4){
             cont =0;
 
         }
