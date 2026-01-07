3058e45d7891d6402c86bc3e6c84ce795109451d
galdinow <galdinow00@pm.me>
2026-01-07 16:38:04 -0300

    removed other menus

--- menu.h
+++ menu.h
@@ -1,8 +1,6 @@
 #ifndef MENU_H
 #define MENU_H
 
-int menu(character *x, character *y);
-int menu_attack(character *x, int cont);
-int  menu_party(character *x, character *y, character *z, character *w);
+int menu(character party[], int length);
 
 #endif
