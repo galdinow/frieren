6b3de2c44c1abda29d0ac4536a8abee26d590723
galdinow <galdinow00@pm.me>
2026-01-07 15:30:15 -0300

    created geader files for menus

---
+++ menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+int menu(character *x, character *y);
+int menu_attack(character *x, int cont);
+int  menu_party(character *x, character *y, character *z, character *w);
+
+#endif
