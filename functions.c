66df677e45afc9e7c950fbbd9070e00fa7f9c262
galdinow <galdinow00@pm.me>
2026-01-07 16:37:42 -0300

    added new functions

--- functions.c
+++ functions.c
@@ -1,18 +1,56 @@
 #include "functions.h"
 
-// void status_party(int *charac, ch frieren, ch warrior, ch heiter){
 
-//     if(frieren.hp <=0){
-//         *charac --;
-//     }
-//     if(warrior.hp <=0){
-//         *charac --;
-//     }
-//     if(heiter.hp <=0){
-//         *charac --;
-//     }
-//     return charac;
-// }
+void update_party(character party[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (party[i].hp == 0)
+        {
+            party[i].alive == false;
+        }
+    }
+    return;
+}
+// checks if the party is dead
+bool party_alive(character party[], int length)
+{
+    int alive = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (party[i].alive == true)
+        {
+            alive++;
+        }
+    }
+    if (alive == length)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool enemy_alive(character enemy)
+{
+    if (enemy.hp > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+void party_attack(character member, character enemy)
+{
+    
+    return;
+}
+
+void enemy_attack(character member, int length, character enemy)
+{
+    
+    return;
+}
 
 
 void kings_turn(int rn_party, int rn_attack, character *frieren, character * himmel, character *heiter, character *eisen, character *king_demon){
@@ -70,23 +108,4 @@ void kings_turn(int rn_party, int rn_attack, character *frieren, character * him
         }
 
     }
-}
-
-
-// checks if the party is dead
-party_status(character party[], int length)
-{
-    int alive = 0;
-    for (int i = 0; i < length; i++)
-    {
-        if (party[i].hp > 0)
-        {
-            alive++;
-        }
-    }
-    if (alive == length)
-    {
-        return true;
-    }
-    return false;
 }
