16c44b882b315fa8f15612fddc90db1d084278f9
galdinow <galdinow00@pm.me>
2026-01-07 16:37:16 -0300

    updated functions and removed useless stats

--- functions.h
+++ functions.h
@@ -5,26 +5,15 @@ typedef struct{
     char name[10];
     int hp;
     int total_hp;
-    int atk;
-    int def;
     int level;
     bool alive;
 } character;
 
-typedef struct{
-    char item1[20];
-    int uses1;
-    char item2[20];
-    int uses2;
-    char item3[20];
-    int uses3;
-
-} bag;
-// status_party(int  *charac, ch frieren, ch warrior, ch heiter);
-
 // colocar array de personagens
 void kings_turn(int rn_party, int rn_attack, character *king_demon);
-bool party_status(character party[], int length);
+void update_party(character party[], int length);
+bool party_alive(character party[], int length);
+bool enemy_alive(character enemy);
 
 
 #endif
