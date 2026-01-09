bf4dec48d57cdf002965cb76ae93b8ac155e7015
galdinow <galdinow00@pm.me>
2026-01-09 02:24:47 -0300

    added party turn

--- main.c
+++ main.c
@@ -5,20 +5,18 @@
 #include <time.h>
 
 
-// character = name, hp, total_hp, level, alive
-character frieren = {"Frieren",200,200,50,50,12, "zoltrak","fireball", "batata", true};
-character eisen={"Eisen",500,500,60,40,14,"cabecada", "furia do anao","soco", true};
-character heiter={"Heiter",200,200,13,53,13,"ataque da deusa", "furia da deusa", "ataque de luz", true};
-character himmel={"Himmel", 300,300,13,53,13, "Avanco do heroi", "charme do heroi", "corte profundo", true};
-character demon_king= {"King",1000,1000,53,53,63,"arroz", "ice ball", "mud", true};
+// character = name, hp, total_hp, alive
+character frieren = {"Frieren",200,200, true};
+character eisen={"Eisen",500,500, true};
+character heiter={"Heiter",200,200, true};
+character himmel={"Himmel", 300,300, true};
+character demon_king= {"King",1000,1000, true};
 
 int main()
 {
-    int escolha;
     const int PARTY_MEMBERS = 4;
     character party[] = {frieren, heiter, eisen, himmel};
 
-    // actual game
     while (true)
     {
         system("clear");
@@ -28,20 +26,24 @@ int main()
 
         if (party_alive(party, PARTY_MEMBERS) == false)
         {
-            printf("You lost! Good luck next time.");
+            printf("You lost! Good luck next time\n.");
             break;
         }
         
         if(enemy_alive(demon_king) == false)
         {
             system("clear");
-            printf("Parabens, voce derrotou o rei demonio. Uma nova era de paz surgira");
+            printf("Congrats! you killed the demon king. A new peace age will appear\n");
             break;
 
         }
 
+        for (int i = 0; i < PARTY_MEMBERS; i++)
+        {
+            menu(party, i, demon_king);
+        }
 
-        kings_turn(frieren, himmel, heiter, eisen, demon_king);
+        kings_turn(party, PARTY_MEMBERS, demon_king);
 
     }
 
