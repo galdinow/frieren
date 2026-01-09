0b23d153a666209457b08399dd6738229c825b48
galdinow <galdinow00@pm.me>
2026-01-09 02:24:25 -0300

    fixed party_alive function and cleaned enemy turn function

--- functions.c
+++ functions.c
@@ -15,17 +15,12 @@ void update_party(character party[], int length)
 // checks if the party is dead
 bool party_alive(character party[], int length)
 {
-    int alive = 0;
     for (int i = 0; i < length; i++)
     {
         if (party[i].alive == true)
         {
-            alive++;
+            return true;
         }
-    }
-    if (alive == length)
-    {
-        return true;
     }
     return false;
 }
@@ -42,7 +37,7 @@ bool enemy_alive(character enemy)
 
 void party_attack(character member, character enemy)
 {
-    
+
     return;
 }
 
@@ -53,59 +48,26 @@ void enemy_attack(character member, int length, character enemy)
 }
 
 
-void kings_turn(int rn_party, int rn_attack, character *frieren, character * himmel, character *heiter, character *eisen, character *king_demon){
-    int heal =0;
-    if(rn_attack == 1){
+void enemy_turn(character party[], int length, character enemy)
+{
+    //ataque em area mas com pouco dano
+    
+    //ataque que se cura em 20% da sua vida total
 
-        if(rn_attack == 1){
-            //ataque em area mas com pouco dano
-            frieren->hp -= 5;
-            himmel->hp -=5;
-            heiter->hp -=5;
-            eisen->hp -=5;
-        }
-    }
-    else if (rn_attack == 0){
-        //ataque que se cura em 20% da sua vida total
-        if(king_demon->hp <king_demon->total_hp && king_demon->hp < king_demon->total_hp*0.5){
-        heal = king_demon->total_hp * 0.2;
-        king_demon->hp +=heal;
-        }
-    }
-    else{
-        if (rn_party == 0){
             // a mesnsagem de qual personagem foi atacado, bem como o dano tomado e seu hp eh mostrado na tela por 3 seg
 
-            if(rn_attack == 2){
                 //ataque de dano medio que retarda o turno do adversario
-                frieren->hp -=30; 
-            }
-        }
-        if (rn_party == 1){
+
             // a mesnsagem de qual personagem foi atacado, bem como o dano tomado e seu hp eh mostrado na tela por 3 seg
 
-            if(rn_attack == 2){
                 //ataque de dano medio que retarda o turno do adversario
-                heiter->hp -=30; 
-            }
-        }
-        if (rn_party == 2){
+
             // a mesnsagem de qual personagem foi atacado, bem como o dano tomado e seu hp eh mostrado na tela por 3 seg
 
-            if(rn_attack == 2){
                 //ataque de dano medio que retarda o turno do adversario
-                eisen->hp -=30; 
-            }
-        }
-        if (rn_party == 3){
+
             // a mesnsagem de qual personagem foi atacado, bem como o dano tomado e seu hp eh mostrado na tela por 3 seg
 
-            if(rn_attack == 2){
                 //ataque de dano medio que retarda o turno do adversario
-                himmel->hp -=30; 
-            }
 
-        }
-
-    }
 }
--- functions.h
+++ functions.h
@@ -1,16 +1,23 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <stdbool.h>
-typedef struct{
+
+typedef struct
+{
+    char *name;
+    int damage; 
+}attack;
+
+typedef struct
+{
     char name[10];
     int hp;
     int total_hp;
-    int level;
     bool alive;
 } character;
 
 // colocar array de personagens
-void kings_turn(int rn_party, int rn_attack, character *king_demon);
+void enemy_turn(character party[], int length, character enemy);
 void update_party(character party[], int length);
 bool party_alive(character party[], int length);
 bool enemy_alive(character enemy);
