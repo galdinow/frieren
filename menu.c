b4e809665186c361a749f801aa0145e2387ed3f4
galdinow <galdinow00@pm.me>
2026-01-31 21:46:16 -0300

    menu overhaul

--- menu.c
+++ menu.c
@@ -1,47 +1,82 @@
+#include <ctype.h>
 #include "menu.h"
 #include "functions.h"
 #include <stdio.h>
+#include <string.h>
+
+int menu_wall = 40;
+
+void menu_header()
+{
+    printf("+");
+    for (int i = 0; i < menu_wall; i++)
+    {   
+        printf("-");
+    }
+    printf("+");
+    printf("\n");
+    return;
+}
+
+
+void show_current_stats(character party[], int turn)
+{
+    char buffer[40];
+
+    sprintf(buffer, "%s - HP: %i/%i", party[turn].name, party[turn].hp, party[turn].total_hp);
+
+    printf(buffer);
+
+    return ;
+}
+
+void show_next_character(character party[], int turn)
+{
+    int buffer[40];
+    sprintf(buffer, "%s - HP: %i/%i", party[turn+1].name, party[turn+1].hp, party[turn+1].total_hp);
+
+    printf(buffer);
+    
+    return;
+}
 
 void menu(character party[], int turn, character enemy)
 {
+    // change this logic later
+    int spaces = 10;
+    
+    show_current_hp(party, turn);
+    
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+
+    show_next_character(party, turn);
+    menu_header();
+
+    printf("Type one of the options to proceed.\n");
+    printf("Action:");
+}
+
+int get_action()
+{
     int action = 0;
-    printf("   ---------------------------------------------------------------------------\n");
-    printf("   | %s- hp: %d\\%d                         next character:%s            |\n", party[turn].name,party[turn].hp,party[turn].total_hp), party[turn + 1].name;
-    printf("   |                           turns until enemy's action:           |\n");
-    printf("   |                                                                         |\n");
-    printf("   |     1 - attack                       2 - see party                      |\n");
-    printf("   |                                                                         |\n");
-    printf("   |     3 - bag                         4 - restart battle                  |\n");
-    printf("   |                                                                         |\n");
-    printf("   |                   enemy's health: %d/%d                             |\n", enemy.hp, enemy.total_hp);
-    printf("   ---------------------------------------------------------------------------\n");
+
     do
     {
-        scanf("%d", &action);
+        scanf("", &action);
     }
-    while (action < 1 || action > 4);
-
-    // if (action == 1)
-    // {
-    //     printf("  ---------------------------------------------------------------------------\n");
-    //     printf("   | %s- hp: %d\\%d                         next character:           |\n", x->name,x->hp,x->total_hp);
-    //     printf("   |     lvl: %d                       turns until enemy's action:           |\n", x->level);
-    //     printf("   |                                                                         |\n");
-    //     printf("   |     1 -%s                     3 -%s                       |\n", x->attack1, x->attack3);
-    //     printf("   |                                                                         |\n");
-    //     printf("   |     2 -%s                      4 -skip                        |\n", x->attack2);
-    //     printf("   ---------------------------------------------------------------------------\n");
-    // }
-
-    // else if (action == 2)
-    // {
-    //     printf("   ---------------------------------------------------------------------------\n");
-    //     printf("   |      frieren-hp: %d/%d                       anao-hp %d/%d              |\n", x->hp, x->total_hp,y->hp,y->total_hp);
-    //     printf("   |                                                                         |\n");
-    //     printf("   |      himmel-hp %d/%d                         heiter-hp: %d/%d           |\n", z->hp, z->total_hp,w->hp, w->total_hp);
-    //     printf("   |                                                                         |\n");
-    //     printf("   |                         4 - skip                                    |\n");
-    //     printf("   ---------------------------------------------------------------------------\n");
-    // }
+    while(action < 1 || action > 5);
     return ;
 }
+
+
+//     printf("   |                           turns until enemy's action:           |\n");
+//     printf("   |                                                                         |\n");
+//     printf("   |     Attack                      Party                      |\n");
+//     printf("   |                                                                         |\n");
+//     printf("   |     Bag                         Reset                  |\n");
+//     printf("   |                                                                         |\n");
+//     printf("   |                   Enemy's health: %d/%d                             |\n", enemy.hp, enemy.total_hp);
+//     printf("   ---------------------------------------------------------------------------\n");
--- menu.h
+++ menu.h
@@ -1,6 +1,6 @@
 #ifndef MENU_H
 #define MENU_H
 #include "functions.h"
-void menu(character party[], int turn, character enemy);
+char *menu(character party[], int turn, character enemy);
 
 #endif
